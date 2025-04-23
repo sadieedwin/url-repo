77dce00d0c8f7fe9be83fdd939653e1cb7b519ee
sadieedwin <sadie_edwin@yahoo.com>
2025-04-23 09:13:40 +0000

    Re-organized files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ubuntu/url_repo/venv
+set -gx VIRTUAL_ENV /home/ubuntu/url-repo/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
